91818bd6d61c07c2cc7e45eafb98d7a74442158b
Muhammad Ammar Hashmi <ammar.hashmi@devops.funsoltech.com>
2026-01-16 09:13:49 +0500

    update run-all.sh

--- bash/ovpn-test-scripts/scripts/run-all.sh
+++ bash/ovpn-test-scripts/scripts/run-all.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 set -e
 
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
+
+# Prevent concurrent cron jobs
+LOCKFILE="/tmp/run-all.lock"
+
+exec 9>"$LOCKFILE"
+if ! flock -n 9; then
+    echo "⛔ Another run-all instance is already running. Exiting."
+    exit 0
+fi
+
+
+# Paths
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+RUN_ALL_LOG_DIR="$SCRIPT_DIR/../run-all-logs"
+
+# -----------------------------
+# Cleanup on ANY exit
+# -----------------------------
+cleanup() {
+    echo
+    echo "[!] Script exited — ensuring LXCs are cleaned up..."
+    "$SCRIPT_DIR/destroy-lxcs.sh" || true
+}
+trap cleanup EXIT
+
+# Make run all script's log directory
+mkdir -p "$RUN_ALL_LOG_DIR"
+
+# Timestamped log file (will be made for each execution)
+LOG_FILE="$RUN_ALL_LOG_DIR/run-all_$(date +%F_%H-%M-%S).log"
+
+# Redirect all stdout/stderr to log
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+# Go to script dir
+cd "$SCRIPT_DIR"
+
 NUM_LXCS=${1:-3}
 # PREFIX=${2:-ovpn-test-}
 
@@ -9,14 +47,31 @@ START_TIME=$(date +%s)
 
 echo "➡️  Starting test run at: $(date)"
 
+# PAUSED TEMPORARILY 
+
+# Fetch fresh ovpn config files
+#"$SCRIPT_DIR/fetch-ovpn-files.sh" || echo "⚠️ Warning: Fetch failed — old files will be used."
+
+# Split .ovpn files based on remotes (OPTIONAL)
+# If you want to enable remote-level cloning of ovpn files, then first do this:
+# In file: start-tests.sh: Update variable named:"OVPN_DIR" from "ovpn-files" to "ovpn-files-split"
+# Uncomment line below if you want remote-level ovpn file cloning
+# "$SCRIPT_DIR/split-ovpn-remotes.sh" 
+
 # Create LXCs
-./launch-lxcs.sh "$NUM_LXCS"
+"$SCRIPT_DIR/launch-lxcs.sh" "$NUM_LXCS"
 
 # Start tests
-./start-tests.sh
+"$SCRIPT_DIR/start-tests.sh"
 
 # Fetch logs
-./fetch-logs.sh
+"$SCRIPT_DIR/fetch-logs.sh"
+
+# Make results readable (OPTIONAL, must enable with "split-ovpn-remotes.sh", otherwise results will be messy)
+# "$SCRIPT_DIR/make-logs-compact.sh"
+
+# Destory LXCs at the end
+"$SCRIPT_DIR/destroy-lxcs.sh"
 
 
 # End timer
@@ -29,10 +84,3 @@ printf -v RUNTIME '%02d:%02d:%02d' \
 
 echo "⏳ Total runtime: $RUNTIME"
 echo "🏁 Completed at: $(date)"
-
-# Ask if user wants to destroy LXCs
-read -p "Do you want to destroy all LXCs? (y/n) " yn
-case $yn in
-    [Yy]* ) ./destroy-lxcs.sh;;
-    * ) echo "LXCs left intact.";;
-esac
